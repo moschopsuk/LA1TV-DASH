c48875c28d33980c0acf8bca70bc06e7edc75359
Luke Moscrop <moschopsuk@gmail.com>
2015-05-16 11:53:18 +0100

    Added configuration file for general site settings

---
+++ config/site.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Site configurations
+ * TODO: Move to a DB to allow easier change to settings
+ */
+
+return array(
+
+	/*
+	 * Configurations for the user
+	 */
+	'users' => [
+		/*
+		 * Whether or not the user has to confirm their email when signing up
+		 */
+		'confirm_email' => true,
+	],
+
+);
+
