5f2db3a05880b723ab9dc01e7b573eb4e1ba25fc
Luke Moscrop <moschopsuk@gmail.com>
2015-05-16 11:52:48 +0100

    Added migration for users

---
+++ database/migrations/2015_05_16_102715_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('email')->unique();
+			$table->string('password', 60)->nullable();
+			$table->string('confirmation_code');
+			$table->boolean('confirmed')->default(config('site.users.confirm_email') ? false : true);
+			$table->rememberToken();
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
